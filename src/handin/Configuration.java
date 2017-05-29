4ac6c18ebaaaf6e2372edfeb05bdbd1489ac84be
Rasmus Skovdal <tokke@tokke.dk>
2017-05-29 12:30:56 +0200

    Caching of the getIP method in the Configuration static class
    Signed-off-by: Rasmus Skovdal <tokke@tokke.dk>

--- src/handin/Configuration.java
+++ src/handin/Configuration.java
@@ -5,21 +5,23 @@ import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Enumeration;
 
-public interface Configuration {
-    int[] portRange = {10000, 10999};
-    int serverPort = 40499;
-    int connectionTimeout = 10000;
+public class Configuration {
+    public static int[] portRange = {10000, 10999};
+    public static int serverPort = 40499;
+    public static int connectionTimeout = 10000;
     // --Commented out by Inspection (21/05/2017 16.44):int connectionAttemptsToNewSequencer = 100;
     // --Commented out by Inspection (21/05/2017 16.44):int waitPerConnectionAttempt = 100;
 
+    private static String ip = null;
     /**
      * Gets the IP address of the current machine.
      * The purpose of this method is to avoid getting a loopback address
      *
      * @return The IP address of the current machine. "127.0.0.1" is returned if no IP is found.
      */
-    static String getIP() {
-        String ip = "127.0.0.1";
+    public static String getIP() {
+        if (ip != null) return ip;
+        ip = "127.0.0.1";
         try {
             Enumeration en = NetworkInterface.getNetworkInterfaces();
             while (en.hasMoreElements()) {
@@ -33,5 +35,30 @@ public interface Configuration {
         } catch (SocketException ignored) {
         }
         return ip;
+    }
+
+    /**
+     * Main method for testing the caching of the getIP() method
+     *
+     * @param args Ignored
+     */
+    public static void main(String[] args) {
+        long start = System.nanoTime();
+        System.out.println("First run:");
+        getIP();
+        int timeFirstRun = Math.toIntExact(System.nanoTime() - start);
+        System.out.println("Time: " + timeFirstRun + "\n");
+
+        System.out.println("Following 10,000 average run:");
+        long sum = 0;
+        for (int i = 0; i < 10000; i++) {
+            long time = System.nanoTime();
+            getIP();
+            sum += (System.nanoTime() - time);
+        }
+        int avg = Math.toIntExact(sum / 10000);
+        System.out.println("Time: " + avg + "\n");
+
+        System.out.println("Caching improved query time by a factor " + timeFirstRun / avg);
     }
 }
